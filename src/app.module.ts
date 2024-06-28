f34687f4e22ed85c921fede0636582fad82a57a9
MateusFels <mateus.fellype14@gmail.com>
2024-06-28 09:21:21 -0300

    new bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './users/users.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { ServiceModule } from './service/service.module';
-import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [UserModule, AppointmentModule, ServiceModule, PaymentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'dpg-cpvagntumphs73c96j90-a',
+      port: 5432,
+      username: 'bd_barber_user',
+      password: 'o6Ea5KctTp8I33El43brdi4VY5Q3EHbU',
+      database: 'bd_barber',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true, 
+    }),
+    UserModule, 
+    AppointmentModule, 
+    ServiceModule
+  ],
   controllers: [],
   providers: [],
 })
